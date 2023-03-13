10a0056c4590aaa6c531f0dbbaf99c32acf7eaa9
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-13 14:46:24 +0530

    Added Unit test Method to  calculate Fare for Multiple Rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -21,9 +21,20 @@ namespace CabInvoiceGeneratorTest
             double fare = invoiceGenerator.CalculateFare(distance, time);
 
             //Assert
-            Assert.AreEqual(expected, fare);
+            Assert.AreEqual(expected, fare);   
         }
 
-       
+        [TestMethod]
+        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expected = new InvoiceSummary(2, 30.0);
+
+            Assert.AreEqual(expected, invoiceSummary);
+        }
     }
 }
